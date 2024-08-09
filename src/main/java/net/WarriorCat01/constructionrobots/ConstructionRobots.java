7907430406302a5e60d5afc4ad4ffbe4eaf18d2c
WarriorCat <tothdavid1016@gmail.com>
2024-08-09 11:00:08 +0200

    "mods.toml missing metadata for modid construcion-robots" Error

--- src/main/java/net/WarriorCat01/constructionrobots/ConstructionRobots.java
+++ src/main/java/net/WarriorCat01/constructionrobots/ConstructionRobots.java
@@ -1,6 +1,8 @@
 package net.WarriorCat01.constructionrobots;
 
 import com.mojang.logging.LogUtils;
+import net.WarriorCat01.constructionrobots.item.Moditems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -18,13 +20,14 @@ import org.slf4j.Logger;
 public class ConstructionRobots
 {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "constructionrobots";
+    public static final String MOD_ID = "construction-robots";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     public ConstructionRobots() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register the commonSetup method for modloading
+        Moditems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -38,7 +41,9 @@ public class ConstructionRobots
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(Moditems.SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
